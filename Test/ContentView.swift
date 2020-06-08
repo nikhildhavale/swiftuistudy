04ad6b92040efbace89e9f1a5977d12949d27afc
Nikhil Dhavale <nikhil_dhavale@hotmail.com>
2020-06-08 15:50:00 +0530

    use of view builder

--- Test/ContentView.swift
+++ Test/ContentView.swift
@@ -32,22 +32,15 @@ struct ContentView: View {
 struct CardView:View
 {
     var card:MemoryGame<String>.Card
+    @ViewBuilder
     var body: some View {
         GeometryReader { geometry in
             ZStack {
-                
-                if self.card.isFaceUp || self.card.isMatched
-                {
-                    RoundedRectangle(cornerRadius: 10.0).stroke().stroke(lineWidth: 2)
-                    RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                    Text(self.card.content)
-                }
-                else if !self.card.isMatched
-                {
-                     RoundedRectangle(cornerRadius: 10.0).fill(Color.orange)
-                }
+                Circle().fill(Color.orange).padding(5).opacity(0.4)
+                Text(self.card.content).font(Font.system(size: min(geometry.size.width, geometry.size.height) * 0.65))
                
-            }.font(Font.system(size: min(geometry.size.width, geometry.size.height) * 0.75) )
+            }.cardify(isFaceUp: self.card.isFaceUp, isMatched: self.card.isMatched)
+                
         }
         
     }
@@ -55,6 +48,40 @@ struct CardView:View
 }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(viewModel: EmojiMemoryGame())
+        let game = EmojiMemoryGame()
+        game.chooseCard(card: game.cards[0])
+        return ContentView(viewModel: game)
+    }
+}
+struct Cardify:ViewModifier
+{
+    var isFaceUp:Bool
+    var isMatched:Bool
+    func body(content: Content) -> some View {
+        ZStack {
+            
+            if isFaceUp || isMatched
+            {
+                RoundedRectangle(cornerRadius: 10.0).stroke().stroke(lineWidth: 2)
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                content
+            }
+            else
+            {
+                RoundedRectangle(cornerRadius: 10.0)                .fill(Color.orange)
+                
+            }
+            
+        }
+        
+    }
+}
+    
+
+extension View
+{
+    func cardify(isFaceUp:Bool,isMatched:Bool) -> some View
+    {
+       modifier(Cardify(isFaceUp: isFaceUp, isMatched: isMatched))
     }
 }
